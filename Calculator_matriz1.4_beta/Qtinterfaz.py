e4cbac2a171a2c3e3294390a1d7bb254a6707fda
CodeR <118649094+brandbold-Ch@users.noreply.github.com>
2022-12-13 00:43:01 -0600

    Add files via upload

--- Calculator_matriz1.4_beta/Qtinterfaz.py
+++ Calculator_matriz1.4_beta/Qtinterfaz.py
@@ -2,10 +2,10 @@ import sys
 from PyQt5.QtWidgets import QWidget, QTabWidget
 from PyQt5 import QtWidgets, uic
 from fractions import Fraction
-import time
+#import time
 from Tab_1 import T1
 
-inicio = time.time()
+#inicio = time.time()
 UIsystem, QtBaseClass = uic.loadUiType("UiMatriz.ui")
 
 class Window(QtWidgets.QMainWindow, UIsystem):
@@ -18,6 +18,7 @@ class Tabs(Window, T1):
     def __init__(self) -> None:
         super().__init__()
         self.tabWidget.currentChanged.connect(self.pages)
+        self.tabWidget.setCurrentIndex(0)
 
     def pages(self):
         match self.tabWidget.currentIndex():
@@ -28,14 +29,28 @@ class Tabs(Window, T1):
             case 1:
                 print("Estoy en la pestaña: 1")
                 self.spinBox2.valueChanged.connect(self.dimensionEcu)
+        
+    def events(self):
+        
+        #Pestaña 0: Determinantes.
+        self.calcular.clicked.connect(self.setValues)
+        self.limpiar.clicked.connect(self.clearValues) 
+        
+        #Pestaña 1: Sistema de ecuaciones.
+        self.calcular2.clicked.connect(self.setValuesE)
+        self.limpiar2.clicked.connect(self.clearValuesE)
+
+        #Grafica de ecuaciones 2x2 y 3x3.
+        self.graficar.clicked.connect(self.graphication2d)    
+        self.graficar2.clicked.connect(self.graphication3d)
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)  # Inicia la aplicacion
-    window = Tabs()  # Se establece una ventana
+    window = Tabs()
+    window.events()# Se establece una ventana
     window.show()
-    print(Window.mro())
-    print(Tabs.mro())
+    #print(Window.mro())
+    #print(Tabs.mro())
     app.exec_()
-    
-    fin = time.time()
-    print(fin-inicio) # 1.5099220275878906
+    #fin = time.time()
+    #print(fin-inicio) # 1.5099220275878906
--- Calculator_matriz1.4_beta/Tab_1.py
+++ Calculator_matriz1.4_beta/Tab_1.py
@@ -5,7 +5,6 @@ from fractions import Fraction
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm 
-#from equation_graph import graphication
 
 """
 Página solucionadora de sistema de ecuaciones,
@@ -16,7 +15,11 @@ class T1(T0):
         super().__init__()
 
     def dimensionEcu(self):
-        self.indexE = [[self.a11__2,self.a21__2,self.a12__2,self.a22__2],[self.a31__2,self.a32__2,self.a13__2,self.a23__2,self.a33__2],[self.a41__2,self.a42__2,self.a43__2,self.a14__2,self.a24__2,self.a34__2,self.a44__2]]
+        self.indexE = [
+                        [self.a11__2,self.a21__2,self.a12__2,self.a22__2],
+                        [self.a31__2,self.a32__2,self.a13__2,self.a23__2,self.a33__2],
+                        [self.a41__2,self.a42__2,self.a43__2,self.a14__2,self.a24__2,self.a34__2,self.a44__2]
+                    ]
         self.equalsE = [self.equal1, self.equal2, self.equal3, self.equal4]
 
         match self.spinBox2.value():
@@ -29,9 +32,8 @@ class T1(T0):
 
                 self.labelig3.hide()
                 self.labelig4.hide()
-
-                self.calcular2.clicked.connect(self.setValuesE)
-                self.limpiar2.clicked.connect(self.clearValuesE)
+                self.graficar.show()
+                self.graficar2.hide()
 
             case 3:
                 for i in self.indexE[2][:]:
@@ -49,9 +51,8 @@ class T1(T0):
                 self.labelig2.show()
                 self.labelig3.show()
                 self.labelig4.hide()
-
-                self.calcular2.clicked.connect(self.setValuesE)
-                self.limpiar2.clicked.connect(self.clearValuesE)
+                self.graficar2.show()
+                self.graficar.hide()
 
             case 4:
                 for i in self.indexE[0][:] + self.indexE[1][:] + self.indexE[2][:]:
@@ -64,55 +65,73 @@ class T1(T0):
                 self.labelig2.show()
                 self.labelig3.show()
                 self.labelig4.show()
+                self.graficar2.hide()
+                self.graficar.hide()
 
     def setValuesE(self):
-        try:
-            match self.spinBox2.value():
-                case 2:
-                    self.listValue = [[float(eval(self.a11__2.toPlainText())), float(eval(self.a21__2.toPlainText()))],[float(eval(self.a12__2.toPlainText())), float(eval(self.a22__2.toPlainText()))]]
+        match self.spinBox2.value():
+            case 2:
+                try:
+                    self.listValue = [
+                                        [float(eval(self.a11__2.toPlainText())), float(eval(self.a21__2.toPlainText()))],
+                                        [float(eval(self.a12__2.toPlainText())), float(eval(self.a22__2.toPlainText()))]
+                                    ]
 
                     self.eq = [float(eval(self.equal1.toPlainText())),float(eval(self.equal2.toPlainText()))]
 
                     self.valuesxy = np.linalg.inv(self.listValue).dot(self.eq)
                     self.x.setText(str(Fraction(self.valuesxy[0]).limit_denominator(100)))
                     self.y.setText(str(Fraction(self.valuesxy[1]).limit_denominator(100)))
-                    
+                        
                     print("Estoy en la ecuación 2x2")
+                except:
+                    messagebox.showerror("Alert", "Error encontrado")
 
-                case 3:
-                    self.listValue = [[float(eval(self.a11__2.toPlainText())), float(eval(self.a21__2.toPlainText())), float(eval(self.a31__2.toPlainText()))],
-                                    [float(eval(self.a12__2.toPlainText())), float(eval(self.a22__2.toPlainText())), float(eval(self.a32__2.toPlainText()))],
-                                    [float(eval(self.a13__2.toPlainText())), float(eval(self.a23__2.toPlainText())), float(eval(self.a33__2.toPlainText()))]]
-                    self.eq = [float(eval(self.equal1.toPlainText())),float(eval(self.equal2.toPlainText())),float(eval(self.equal3.toPlainText()))]
+            case 3:
+                try:
+                    self.listValue = [
+                                        [float(eval(self.a11__2.toPlainText())), float(eval(self.a21__2.toPlainText())), float(eval(self.a31__2.toPlainText()))],
+                                        [float(eval(self.a12__2.toPlainText())), float(eval(self.a22__2.toPlainText())), float(eval(self.a32__2.toPlainText()))],
+                                        [float(eval(self.a13__2.toPlainText())), float(eval(self.a23__2.toPlainText())), float(eval(self.a33__2.toPlainText()))]
+                                    ]
+                    self.eq = [
+                                float(eval(self.equal1.toPlainText())),
+                                float(eval(self.equal2.toPlainText())),
+                                float(eval(self.equal3.toPlainText()))
+                            ]
 
                     self.valuesxy = np.linalg.inv(self.listValue).dot(self.eq)
                     self.x.setText(str(Fraction(self.valuesxy[0]).limit_denominator(100)))
                     self.y.setText(str(Fraction(self.valuesxy[1]).limit_denominator(100)))
                     self.z.setText(str(Fraction(self.valuesxy[2]).limit_denominator(100)))
-                    
-                    self.graficar.clicked.connect(self.graphication)
+                        
                     print("Estoy en la ecuación 3x3")
-                    
-                case 4:
-                    self.listValue = [[float(eval(self.a11__2.toPlainText())), float(eval(self.a21__2.toPlainText())), float(eval(self.a31__2.toPlainText())), float(eval(self.a41__2.toPlainText()))],
-                                    [float(eval(self.a12__2.toPlainText())), float(eval(self.a22__2.toPlainText())), float(eval(self.a32__2.toPlainText())), float(eval(self.a42__2.toPlainText()))],
-                                    [float(eval(self.a13__2.toPlainText())), float(eval(self.a23__2.toPlainText())), float(eval(self.a33__2.toPlainText())), float(eval(self.a43__2.toPlainText()))],
-                                    [float(eval(self.a14__2.toPlainText())), float(eval(self.a24__2.toPlainText())), float(eval(self.a34__2.toPlainText())), float(eval(self.a44__2.toPlainText()))]]
-                    self.eq = [float(eval(self.equal1.toPlainText())),float(eval(self.equal2.toPlainText())),float(eval(self.equal3.toPlainText())),float(eval(self.equal4.toPlainText()))]
+                except:
+                    messagebox.showerror("Alert", "Error encontrado")
+
+            case 4:
+                try:
+                    self.listValue = [
+                                        [float(eval(self.a11__2.toPlainText())), float(eval(self.a21__2.toPlainText())), float(eval(self.a31__2.toPlainText())), float(eval(self.a41__2.toPlainText()))],
+                                        [float(eval(self.a12__2.toPlainText())), float(eval(self.a22__2.toPlainText())), float(eval(self.a32__2.toPlainText())), float(eval(self.a42__2.toPlainText()))],
+                                        [float(eval(self.a13__2.toPlainText())), float(eval(self.a23__2.toPlainText())), float(eval(self.a33__2.toPlainText())), float(eval(self.a43__2.toPlainText()))],
+                                        [float(eval(self.a14__2.toPlainText())), float(eval(self.a24__2.toPlainText())), float(eval(self.a34__2.toPlainText())), float(eval(self.a44__2.toPlainText()))]
+                                    ]
+                    self.eq = [
+                                    float(eval(self.equal1.toPlainText())),float(eval(self.equal2.toPlainText())),
+                                    float(eval(self.equal3.toPlainText())),float(eval(self.equal4.toPlainText()))
+                                ]
 
                     self.valuesxy = np.linalg.inv(self.listValue).dot(self.eq)
                     self.x.setText(str(Fraction(self.valuesxy[0]).limit_denominator(100)))
                     self.y.setText(str(Fraction(self.valuesxy[1]).limit_denominator(100)))
                     self.z.setText(str(Fraction(self.valuesxy[2]).limit_denominator(100)))
                     self.w.setText(str(Fraction(self.valuesxy[3]).limit_denominator(100)))
-                    
-                    print("Estoy en la ecuación 3x3")
-
-        except:
-            messagebox.showerror("Alert", "Error encontrado")
-        finally:
-            print("Entre en una excepción")
-
+                        
+                    print("Estoy en la ecuación 4x4")
+                except:
+                    messagebox.showerror("Alert", "Error encontrado")
+            
     def clearValuesE(self):
         match self.spinBox2.value():
             case 2:
@@ -150,46 +169,81 @@ class T1(T0):
                 self.z.setText("")
                 self.w.setText("")
                 
-    def graphication(self):
-        
-        equation_system = self.listValue
-        solution = self.valuesxy
-        
-        x, y = np.linspace(0,10,10), np.linspace(0,10,10)
-        X, Y = np.meshgrid(x, y)
-        
-        for i in range(len(equation_system)-1):
-            if equation_system[0][i]>0 or equation_system[0][i]<0:
-                equation_system[0][i] = equation_system[0][i]*-1
-        
-        z1 = (solution[0] + equation_system[0][0]*X + equation_system[0][1]*Y)/equation_system[0][2]
-        
-        for j in range(len(equation_system)-1):
-            if equation_system[1][j]>0 or equation_system[1][j]<0:
-                equation_system[1][j] = equation_system[1][j]*-1
-        
-        z2 = (solution[1] + equation_system[1][0]*X + equation_system[1][1]*Y) / equation_system[1][2]
-        
-        for k in range(len(equation_system)-1):
-            if equation_system[2][k]>0 or equation_system[2][k]<0:
-                equation_system[2][k] = equation_system[2][k]*-1
-        
-        z3 = (solution[2] + equation_system[2][0]*X +equation_system[2][1]*Y)/equation_system[2][2]
-                
-        for a in range(len(equation_system)):
-            for b in range(len(equation_system)):
-                
-                print(equation_system[a][b],", ")
-                  
-        fig = plt.figure()
-        
-        ax = fig.add_subplot(111,projection="3d")
-        
-        ax.plot_surface(X,Y,z1, alpha=0.5, cmap=cm.Accent, rstride=100, cstride=100)
-        ax.plot_surface(X,Y,z2, alpha=0.5, cmap=cm.Paired, rstride=100, cstride=100)
-        ax.plot_surface(X,Y,z3, alpha=0.5, cmap=cm.Pastel1, rstride=100, cstride=100)
-        
-        ax.plot((solution[0],), (solution[1],), (solution[2],), lw=2, c="k", marker="o",markersize=7, markeredgecolor="g", markerfacecolor="white")
+    def graphication2d(self):
+        try:
+            x = np.linspace(-5,5,100)
+            a = self.listValue
+            b = self.eq
 
-        ax.set_xlabel("X"); ax.set_ylabel("Y"); ax.set_zlabel("Z")
-        plt.show()
+            #Despeje de ecuciones para Y
+            for i in range(len(a)):
+                print("")
+                for j in range(len(a)-1):
+                    a[i][j] *= -1
+
+            for q in range(len(a)):
+                print("")
+                for w in range(len(a)):
+                    print(a[q][w])
+
+            y1 = (b[0] + a[0][0]*x) / a[0][1]
+            y2 = (b[1] + a[1][0]*x) / a[1][1]
+
+            f1 = f"y1 = ({b[0]} {a[0][0]}*x) / {a[0][1]}"
+            f2 = f"y2 = ({b[1]} {a[1][0]}*x) / {a[1][1]}"
+
+            plt.plot(x, y1, c="r", label=f1)
+            plt.plot(x, y2, c="b", label=f2)
+
+            plt.title('Gráfica de sistema de ecuaciones')
+
+            plt.xlabel('x', color='#1C2833'); plt.ylabel('y', color='#1C2833'); plt.legend(loc='upper left')
+
+            plt.grid()
+            plt.show()
+        except:
+            messagebox.showerror("Alert", "Error encontrado")
+
+                
+    def graphication3d(self):
+        try:
+            equation_system = self.listValue
+            solution = self.valuesxy
+            equals = self.eq
+            
+            print(equation_system)
+            print(equals)
+            print(solution)
+            
+            x, y = np.linspace(0,50,50), np.linspace(0,50,50)
+            X, Y = np.meshgrid(x, y)
+            
+            #Despeje de ecuaiciones para Z
+            for i in range(len(equation_system)):
+                for j in range(len(equation_system)-1):
+                    if equation_system[i][j] > 0 or equation_system[i][j] < 0:
+                        equation_system[i][j] *= -1
+                        
+            z1 = (equals[0] + equation_system[0][0]*X + equation_system[0][1]*Y)/equation_system[0][2]
+            z2 = (equals[1] + equation_system[1][0]*X + equation_system[1][1]*Y) /equation_system[1][2]
+            z3 = (equals[2] + equation_system[2][0]*X +equation_system[2][1]*Y)/equation_system[2][2]
+            
+            for a in range(len(equation_system)):
+                print("")
+                for b in range(len(equation_system)):    
+                    print(equation_system[a][b], end=", ")
+                    
+            fig = plt.figure()
+            
+            ax = fig.add_subplot(111,projection="3d")
+
+            ax.plot_surface(X,Y,z1, alpha=0.5, cmap=cm.Accent, rstride=100, cstride=100)
+            ax.plot_surface(X,Y,z2, alpha=0.5, cmap=cm.Paired, rstride=100, cstride=100)
+            ax.plot_surface(X,Y,z3, alpha=0.5, cmap=cm.Pastel1, rstride=100, cstride=100)
+            
+            ax.plot((solution[0],), (solution[1],), (solution[2],), lw=2, c="k", marker="o",markersize=7, markeredgecolor="g", markerfacecolor="white")
+
+            ax.set_xlabel("X"); ax.set_ylabel("Y"); ax.set_zlabel("Z")
+            plt.show()
+        except:
+            messagebox.showerror("Alert", "Error encontrado")
